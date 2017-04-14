a30f08f2ea878127d6eff910a063af49900bf943
Oliver <oliver.mtz10@gmail.com>
2017-04-14 00:55:23 -0500

    Ya se generan resultados de expresiones aritmeticas y print

---
+++ tests/expresiones.mm
@@ -0,0 +1,12 @@
+program polaco:
+{	int ia;
+	int ib;
+	string ic;
+	ib = 7;
+	ia = 4.3 * ib + 9 + 8 * 2 / 3 + 1 * 5 * (5 + 1 + 5 - 3) + 1;
+	print("El resultado de la operacion es: ");
+	print(ia);
+	ic = "Ya funciona el programa!!!. Genial";
+	print (ic);
+		
+}
